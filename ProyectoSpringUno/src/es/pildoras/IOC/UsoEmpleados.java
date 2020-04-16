b3a7a382e9a19c7a87a7e7a14c270315a31200ce
oscar_9414 <oscar9414ps@gmail.com>
2020-04-16 11:25:19 +0200

    Video 8 de Spring- Creacion del applicationContext.xml y exmplicación de su función.

--- ProyectoSpringUno/src/es/pildoras/IOC/UsoEmpleados.java
+++ ProyectoSpringUno/src/es/pildoras/IOC/UsoEmpleados.java
@@ -1,5 +1,7 @@
 package es.pildoras.IOC;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * 
  * @author oscar
@@ -13,15 +15,29 @@ public class UsoEmpleados {
 	public static void main(String[] args) {
 		
 		//Creación de objetos de tipo Empleado
-		JefeEmpleado empleados1 = new JefeEmpleado();
-		JefeEmpleado empleados2 = new SecretarioEmpleado();
-		JefeEmpleado empleados3 = new DirectorEmpleado();
+//		JefeEmpleado empleados1 = new JefeEmpleado();
+//		JefeEmpleado empleados2 = new SecretarioEmpleado();
+//		JefeEmpleado empleados3 = new DirectorEmpleado();
+//		
+//		//Uso de los objetos creados
+//		System.out.println(empleados1.getTareas());
+//		System.out.println(empleados2.getTareas());
+//		System.out.println(empleados3.getTareas());
 		
-		//Uso de los objetos creados
-		System.out.println(empleados1.getTareas());
-		System.out.println(empleados2.getTareas());
-		System.out.println(empleados3.getTareas());
+		//primer paso: crear el contexto
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//Segundo paso: Pedrile al archivo xml el bean
+		Empleados oscar = contexto.getBean("miEmpleado", Empleados.class);
 		
+		System.out.println(oscar.getTareas());
+		
+		//Último paso: cerrar el conetxto
+		contexto.close();
+		
+		/*La verdadera ultilidad de esto es: 
+			si en algun momento necesito onjetos de otro tipo no tengo que crear el objeto, crear la instancia, usar la instacia..args
+			Simplemente hayq ue ir al archivo xml y decirle el tipo que se desea devolver
+			*/
 	}
 
 }
